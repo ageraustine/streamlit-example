d5586e5512e2d627f5604383740b1f27e89b4a73
Ager Austine <86380543+ageraustine@users.noreply.github.com>
2024-03-29 13:09:37 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,5 +1,7 @@
 import requests
+import os
 
+API_URL = os.environ["API_URL"]
 def query(payload, headers):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
