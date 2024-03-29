48b5d8079af6eb5c5d3517365dce87dbc439f40c
Ager Austine <86380543+ageraustine@users.noreply.github.com>
2024-03-29 12:48:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,32 +3,36 @@ import requests
 import numpy as np
 import os
 
-API_URL =  os.environ["API_URL"]
+# Try to get API_URL from environment variables, if not found set to a default value
+try:
+    API_URL = os.environ["API_URL"]
+except KeyError:
+    st.error("API_URL environment variable is not set.")
+    st.stop()
 
 headers = {
-	"Authorization": "Bearer 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",
-	"Content-Type": "application/json"
+    "Authorization": "Bearer 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",
+    "Content-Type": "application/json"
 }
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
 # Streamlit app
 st.title("Songlabai")
 
 # Input prompt from the user
 user_prompt = st.text_input("Enter your prompt:", "")
+
 # Input form
 duration = st.number_input("Duration (in seconds)", min_value=10)
 
-
 # Generate audio based on the user's prompt
 if st.button("Generate Audio"):
-        if user_prompt and duration:
-            st.text("Generating audio...")
-            response = query({"inputs": {"prompt":user_prompt,"duration":int(duration)}})
-            audio = np.array(response[0]['generated_audio'])
-	    audio = np.float32(audio)
-            sample_rate = response[0]['sample_rate']
-            st.audio(audio, format="audio/mp3", sample_rate=sample_rate)
+    if user_prompt and duration:
+        st.text("Generating audio...")
+        response = query({"inputs": {"prompt": user_prompt, "duration": int(duration)}})
+        audio = np.array(response[0]['generated_audio'], dtype=np.float32)
+        sample_rate = response[0]['sample_rate']
+        st.audio(audio, format="audio/wav", sample_rate=sample_rate)
