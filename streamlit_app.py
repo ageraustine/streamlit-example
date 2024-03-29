9a93ff5a20e74b390b0766a22adf10e8772eafd2
Ager Austine <86380543+ageraustine@users.noreply.github.com>
2024-03-29 12:41:23 +0300

    URL as an env

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import requests
 import numpy as np
-API_URL = "https://j7q348ccukmo1z0o.us-east-1.aws.endpoints.huggingface.cloud"
+import os
+
+API_URL =  os.environ["API_URL"]
 
 headers = {
 	"Authorization": "Bearer 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",
@@ -27,5 +29,6 @@ if st.button("Generate Audio"):
             st.text("Generating audio...")
             response = query({"inputs": {"prompt":user_prompt,"duration":int(duration)}})
             audio = np.array(response[0]['generated_audio'])
+	    audio = np.float32(audio)
             sample_rate = response[0]['sample_rate']
             st.audio(audio, format="audio/mp3", sample_rate=sample_rate)
